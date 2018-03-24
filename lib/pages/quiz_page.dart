39e97ee0b19afd13a789f36cbce3a6545f826df7
Phillip Mwaniki Nzuli <mwanikiphillip@gmail.com>
2018-03-24 11:48:20 +0300

    created question_text ui element

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import '../utils/question.dart';
 import '../utils/quiz.dart';
 import '../ui/answer_button.dart';
+import '../ui/question_text.dart';
+
 
 class QuizPage extends StatefulWidget {
   @override
@@ -21,8 +23,9 @@ class QuizPageState extends State<QuizPage> {
       children: <Widget>[
         new Column(
           children: <Widget>[
-            new AnswerButton(true),
-            new AnswerButton(false)
+            new AnswerButton(true, () => print("True")),
+            new QuestionText("Pizza is nice", 1),
+            new AnswerButton(false,  () => print("False"))
           ],
         )
       ],
--- lib/ui/answer_button.dart
+++ lib/ui/answer_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget{
 
-  bool _answer;
+  final bool _answer;
+  final VoidCallback _onTap;
 
-  AnswerButton(this._answer);
+  AnswerButton(this._answer, this._onTap);
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +16,15 @@ class AnswerButton extends StatelessWidget{
         child: new Material(
           color: _answer == true ? Colors.greenAccent : Colors.redAccent,
           child: new InkWell(
-            onTap: () => print(_answer == true ? "true" : "false"),
+            onTap: () => _onTap,
             child: new Center(
               child: new Container(
-                child: new Text(_answer == true ? "true" : "false"),
+                decoration: new BoxDecoration(
+                  border: new Border.all(color: Colors.white, width: 5.0),
+                ),
+                padding: new EdgeInsets.all(20.0),
+                child: new Text(_answer == true ? "true" : "false",
+                style: new TextStyle(color: Colors.white, fontSize: 40.0, fontStyle: FontStyle.italic),),
               ),
             ),
           ),
